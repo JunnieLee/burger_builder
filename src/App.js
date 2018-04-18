9c1f751e3cfb88fbc82c009bafb2351f19da13ab
JunnieLee <heunghari@naver.com>
2018-04-18 17:18:17 +0900

    state property in class components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+        // state is managed inside a component
+        // whereas props is set and passed from outside of the component
+        // Also, state is only usable in components that extends { Component } from 'react'.
+        // So it is not available in function components (like the Person component)
+  state = {
+    students : [
+      {name: 'Junnie', age: 23},
+      {name: 'Harnie', age: 20},
+      {name: 'Jennifer', age: 23}
+    ]
+  }  
+  // you can define other properties as well, but still, state is a very special one.    
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app! :) </h1>
         <p>Yay! This is really working!</p>
-        <Person name="Junnie" age="23" />
-        <Person name="Harnie" age="20">Hobby: Sleeping</Person>
-        <Person name="Jennifer" age="23"/>
+        <Person name={this.state.students[0].name} age={this.state.students[0].age} />
+        <Person name={this.state.students[1].name} age={this.state.students[1].age}>Hobby: Sleeping</Person>
+        <Person name={this.state.students[2].name} age={this.state.students[2].age}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react'; // 얠 데려와야 jsx문법을 유효하게 사용할 수 있음!!
 
+// This is a function component
 const person = (props) => { // props argument를 pass 해줘야 다른데서 이 아이의 property에 자유로이 접근 및 정의?할 수 있음!
 	return (
 		<div>
 			<p>I'm {props.name}! and I am {props.age} years old!</p>
 			<p>{props.children}</p> 
 		</div>					// children이란 term은 정해져있는 약속임. component 태그 사이에 들어가는 애들을 지칭함.
-		)						// children은 plain text뿐 아니라 jsx코드나 다른 react component가 될수도 있음.
+		)						// children은 plain text뿐 아니라 html 형태의 jsx코드나 다른 react component가 될수도 있음.
 
 };                                   
 // * when using a class-based component, it's this.props 
